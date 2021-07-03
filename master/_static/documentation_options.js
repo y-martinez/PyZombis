a9cfbadd88d83b6699557988a4bd9ed193da9103
y-martinez <y-martinez@users.noreply.github.com>
2021-07-03 22:41:15 +0000

    deploy: 4096289f566445681fda5bbbb60d977ef1098c93

--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'c7987c0c028d5d111b14a1fd9440d88d83471894-master',
+    VERSION: '4096289f566445681fda5bbbb60d977ef1098c93-master',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
